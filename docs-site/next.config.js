3584135c97fbed6aa2f58acd7238ef23a73076e8
Ary0520 <aryansinhag2006@gmail.com>
2026-08-23 01:55:49 +0530

    trynna fix vercel dep monorepo issue

--- docs-site/next.config.js
+++ docs-site/next.config.js
@@ -1,8 +1,16 @@
+const path = require('path');
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx'
 })
 
 module.exports = withNextra({
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '/')
+  }
 })
